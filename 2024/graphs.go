38037f7a4bce790dad2b28230159c9bff2409064
Aled Diplock <aled.diplock@tessella.com>
2024-11-05 14:37:21 +0000

    Simplified graph

--- 2024/graphs.go
+++ 2024/graphs.go
@@ -5,69 +5,28 @@ import (
 )
 
 func buildBarGraph() []byte {
-	values := [][]float64{
+	// based on package's example https://github.com/vicanso/go-charts/blob/main/examples/bar_chart/main.go
+
+	run_times := [][]float64{
 		{
 			2.0,
 			4.9,
-			7.0,
-			23.2,
-			25.6,
-			76.7,
-			135.6,
-			162.2,
-			32.6,
-			20.0,
-			6.4,
-			3.3,
 		},
 		{
 			2.6,
 			5.9,
-			9.0,
-			26.4,
-			28.7,
-			70.7,
-			175.6,
-			182.2,
-			48.7,
-			18.8,
-			6.0,
-			2.3,
 		},
 	}
 	p, err := charts.BarRender(
-		values,
+		run_times,
 		charts.XAxisDataOptionFunc([]string{
-			"Jan",
-			"Feb",
-			"Mar",
-			"Apr",
-			"May",
-			"Jun",
-			"Jul",
-			"Aug",
-			"Sep",
-			"Oct",
-			"Nov",
-			"Dec",
+			"Day 1",
+			"Day 2",
 		}),
 		charts.LegendLabelsOptionFunc([]string{
-			"Rainfall",
-			"Evaporation",
+			"Part 1",
+			"Part 2",
 		}, charts.PositionRight),
-		charts.MarkLineOptionFunc(0, charts.SeriesMarkDataTypeAverage),
-		charts.MarkPointOptionFunc(0, charts.SeriesMarkDataTypeMax,
-			charts.SeriesMarkDataTypeMin),
-		// custom option func
-		func(opt *charts.ChartOption) {
-			opt.SeriesList[1].MarkPoint = charts.NewMarkPoint(
-				charts.SeriesMarkDataTypeMax,
-				charts.SeriesMarkDataTypeMin,
-			)
-			opt.SeriesList[1].MarkLine = charts.NewMarkLine(
-				charts.SeriesMarkDataTypeAverage,
-			)
-		},
 	)
 	if err != nil {
 		panic(err)
