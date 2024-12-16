d3477ac217b7886509a0a504ef420a8e4939eab8
Aled Diplock <aled.diplock@tessella.com>
2024-12-16 21:33:34 +0000

    not quite working day16 p2

--- 2024/16.go
+++ 2024/16.go
@@ -18,10 +18,13 @@ func day16(part2 bool) Solution {
 			Part1_16(input_filepath),
 		}
 	} else {
+		example_p2 := Part2_16(example_filepath)
+		AssertExample("45", example_p2, 2)
+		AssertExample("64", Part2_16("./inputs/16/example2.txt"), 2)
 		return Solution{
 			"16",
-			"example part 2",
-			"input part 2",
+			example_p2,
+			Part2_16(input_filepath),
 		}
 	}
 }
@@ -31,6 +34,7 @@ type Position16 struct {
 	x    int
 	dir  int
 	cost int
+	prev [][2]int
 }
 
 // Heap Queue based on example on: https://pkg.go.dev/container/heap
@@ -73,7 +77,7 @@ outer:
 
 	// STARTS FACING LEFT
 	// following my classic 0 UP 1 RIGHT 2 DOWN 3 LEFT approach
-	start := Position16{sy, sx, 3, 0} // y x dir cost
+	start := Position16{sy, sx, 3, 0, [][2]int{}} // y x dir cost
 	queue := Position16Heap{start}
 	heap.Init(&queue)
 	var best_cost int
@@ -153,8 +157,147 @@ outer:
 				continue
 			}
 			seen[_loc] = true
-			queue = append(queue, Position16{ny, nx, i, item.cost + cost})
+			queue = append(queue, Position16{ny, nx, i, item.cost + cost, [][2]int{}})
 		}
 	}
 	return fmt.Sprintf("%d", best_cost)
+}
+
+func Part2_16(filepath string) string {
+	/*
+
+		probably gonna want to find one path at a time, until it's no longer == to best score
+		so create a seen set that includes DIR AND SCORE of original path, to avoid redoing it
+		something like that
+
+
+
+
+		.... TODO
+
+	*/
+	grid := readStringGrid(filepath)
+	var sy, sx int
+outer:
+	for y, row := range grid {
+		for x, char := range row {
+			if char == "S" {
+				sy, sx = y, x
+				break outer
+			}
+		}
+	}
+	if sy == 0 || sx == 0 {
+		panic("couldn't find start or end")
+	}
+
+	// STARTS FACING LEFT
+	// following my classic 0 UP 1 RIGHT 2 DOWN 3 LEFT approach
+	start := Position16{sy, sx, 3, 0, [][2]int{{sy, sx}}} // y x dir cost
+	queue := Position16Heap{start}
+	heap.Init(&queue)
+	seen := make(map[[3]int]int) // y x dir
+	var best_cost int
+	best_points := make(map[[2]int]bool)
+	first := true
+	for queue.Len() > 0 {
+		item := heap.Pop(&queue).(Position16)
+		if grid[item.y][item.x] == "E" {
+			if first || item.cost == best_cost {
+				best_cost = item.cost
+				for _, pos := range item.prev {
+					best_points[pos] = true
+				}
+				first = false
+			}
+		}
+	dir_loop:
+		for i := range 4 {
+			// 0 1 2 3
+			var ny, nx int
+			var cost int
+			switch i {
+			case 0:
+				// up
+				ny, nx = item.y-1, item.x
+				switch item.dir {
+				case 0:
+					cost = 1
+				case 1:
+					cost = 1001
+				case 2:
+					continue dir_loop
+				case 3:
+					cost = 1001
+				}
+			case 1:
+				// right
+				ny, nx = item.y, item.x+1
+				switch item.dir {
+				case 0:
+					cost = 1001
+				case 1:
+					cost = 1
+				case 2:
+					cost = 1001
+				case 3:
+					continue dir_loop
+				}
+			case 2:
+				// down
+				ny, nx = item.y+1, item.x
+				switch item.dir {
+				case 0:
+					continue dir_loop
+				case 1:
+					cost = 1001
+				case 2:
+					cost = 1
+				case 3:
+					cost = 1001
+				}
+			case 3:
+				// left
+				ny, nx = item.y, item.x-1
+				switch item.dir {
+				case 0:
+					cost = 1001
+				case 1:
+					continue dir_loop
+				case 2:
+					cost = 1001
+				case 3:
+					cost = 1
+				}
+			default:
+				panic("bad dir")
+			}
+			if ny < 0 || ny >= len(grid) || nx < 0 || nx >= len(grid[0]) || grid[ny][nx] == "#" {
+				continue
+			}
+			_loc := [3]int{ny, nx, i}
+			if seen_cost, have_seen := seen[_loc]; have_seen && seen_cost > 150 {
+				continue
+			}
+			seen[_loc]++
+			var new_prev [][2]int
+			new_prev = append(new_prev, item.prev...)
+			new_prev = append(new_prev, [2]int{ny, nx})
+			queue = append(queue, Position16{ny, nx, i, item.cost + cost, new_prev})
+		}
+	}
+
+	fmt.Println()
+	for y, line := range grid {
+		fmt.Println()
+		for x, char := range line {
+			if _, in := best_points[[2]int{y, x}]; in {
+				fmt.Print(GREEN_FORE, " O ", RESET)
+			} else {
+				fmt.Printf(" %v ", char)
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", len(best_points))
 }
