78f77b20f4efc2b6b5fa955e21699ed2670e5f82
Aled Diplock <aled.diplock@tessella.com>
2024-12-19 11:29:39 +0000

    p1 checkpoint

--- 2024/19.go
+++ 2024/19.go
@@ -17,9 +17,10 @@ func day19(part2 bool) Solution {
 			Part1_19(input_filepath),
 		}
 	} else {
+		example_p2 := Part2_19(example_filepath)
 		return Solution{
 			"19",
-			"example part 2",
+			example_p2,
 			"input part 2",
 		}
 	}
@@ -34,28 +35,25 @@ func ParseDay19(filepath string) (options []string, targets []string) {
 }
 
 func IsViableTowelCombo(target string, options []string) bool {
-	// fmt.Println("LOOKING FOR: ", target)
-
 	pointer := 0
 	tried := make([][]int, len(target))
-	for j := range tried {
-		tried[j] = make([]int, len(options))
+	for i := range tried {
+		tried[i] = make([]int, len(options))
 	}
+
+	var previous_pointer int
+	var previous_j int
 
 	for {
 		if pointer == len(target) {
-			// fmt.Println("FOUND TARGET!", target)
 			return true
 		}
-		// fmt.Println(pointer, target[pointer:])
 		sub_found := false
 		if RowFilledWithOnes(tried, pointer) {
 			return false
 		}
 		for j, option := range options {
-			// fmt.Println("option:", option)
 			if tried[pointer][j] == 1 {
-				// fmt.Println("already tried")
 				continue
 			}
 			n := len(option)
@@ -64,18 +62,20 @@ func IsViableTowelCombo(target string, options []string) bool {
 				continue
 			}
 			sub_target := target[pointer : pointer+n]
-			tried[pointer][j] = 1
 
-			// fmt.Println("sub:", sub_target, "option", option)
 			if sub_target == option {
-				// fmt.Println("sub == option!")
-				pointer += n
 				sub_found = true
+				previous_j = j
+				previous_pointer = pointer
+				pointer += n
 				break
 			}
 		}
 		if !sub_found {
-			// fmt.Println("not sub found, tried=", tried)
+			tried[previous_pointer][previous_j] = 1 // this combo caused issue, so dont allow it!
+			if pointer == 0 {
+				return false
+			}
 			pointer = 0
 		}
 	}
@@ -101,4 +101,87 @@ func Part1_19(filepath string) string {
 	}
 
 	return fmt.Sprintf("%d", count)
+}
+
+/*
+  - recursive solution where I fire off into a new one each time found,
+    instead of the whole pointer moving
+
+- so for each 'layer' [pointer] find all valid matches, each += into an isValid
+
+  - some sort of deduplication, maybe keep track of the 'path' and fill those spots
+    in the tried map to block redoing it...? or return all paths then dedup... not sure
+*/
+func Part2_19(filepath string) string {
+	options, targets := ParseDay19(filepath)
+
+	var count int
+	for _, target := range targets {
+		count += NumViableTowelCombo(target, options)
+	}
+
+	return fmt.Sprintf("%d", count)
+}
+
+func NumViableTowelCombo(target string, options []string) int {
+	var total int
+
+	tried := make([][]int, len(target))
+	for i := range tried {
+		tried[i] = make([]int, len(options))
+	}
+
+	for {
+		pointer := 0
+		previous_pointer := 0
+		previous_j := 0
+		valid := false
+	attempt_loop:
+		for {
+			if pointer == len(target) {
+				valid = true
+				break attempt_loop
+			}
+			sub_found := false
+			if RowFilledWithOnes(tried, pointer) {
+				valid = false
+				break attempt_loop
+			}
+			for j, option := range options {
+				if tried[pointer][j] == 1 {
+					continue
+				}
+				n := len(option)
+				if pointer+n > len(target) {
+					tried[pointer][j] = 1
+					continue
+				}
+				sub_target := target[pointer : pointer+n]
+
+				if sub_target == option {
+					sub_found = true
+					previous_j = j
+					previous_pointer = pointer
+					pointer += n
+					break
+				}
+			}
+			if !sub_found {
+				tried[previous_pointer][previous_j] = 1 // this combo caused issue, so dont allow it!
+				if pointer == 0 {
+					valid = false
+					break attempt_loop
+				}
+				pointer = 0
+			}
+		}
+		// out of attempt loop
+		if !valid {
+			// hit an invalid case, no more valid cases remain?
+			return total
+		} else {
+			// TODO avoid treading previous path!
+			total++
+		}
+	}
 }
